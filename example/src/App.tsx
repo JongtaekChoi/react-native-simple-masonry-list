2fffd65911b5ff5196e4ae8674984da34f4e24c5
Jongtaek Choi <lastchoice86@naver.com>
2022-03-05 03:05:48 +0900

    Create MasonryList and example

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,31 +1,110 @@
 import * as React from 'react';
 
-import { StyleSheet, View, Text } from 'react-native';
-import { multiply } from 'react-native-simple-masonry-list';
+import {
+  Dimensions,
+  Image,
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native';
+
+import MasonryList from 'react-native-simple-masonry-list';
+import { UNSPLASH_ACCESS_KEY } from '../env.json';
+
+interface UnsplashImage {
+  id: string;
+  width: number;
+  height: number;
+
+  urls: {
+    small: string;
+  };
+  user: {
+    name: string;
+  };
+}
+
+async function fetchUnsplash(
+  path: string,
+  params: Record<string, string | number> = {}
+): Promise<UnsplashImage[]> {
+  const paramsString = Object.keys(params)
+    .map((key) => `${key}=${params[key]}`)
+    .join('&');
+  return fetch(encodeURI(`https://api.unsplash.com/${path}?${paramsString}`), {
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json',
+      'Authorization': `Client-ID ${UNSPLASH_ACCESS_KEY}`,
+    },
+  }).then((response) => {
+    if (response.ok) return response.json();
+    throw new Error(`Fetch failed with code ${response.status}.`);
+  });
+}
 
 export default function App() {
-  const [result, setResult] = React.useState<number | undefined>();
+  const itemWidth = Dimensions.get('screen').width / 2 - 20;
+  const [images, setImages] = React.useState<
+    Array<{ image: UnsplashImage; height: number }>
+  >([]);
 
   React.useEffect(() => {
-    multiply(3, 7).then(setResult);
+    fetchUnsplash('photos/random', { count: 30 }).then((result) => {
+      setImages(
+        result.map((image) => ({
+          image,
+          height: (itemWidth * image.height) / image.width + 24,
+        }))
+      );
+    });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   return (
-    <View style={styles.container}>
-      <Text>Result: {result}</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <MasonryList
+        style={styles.masonryList}
+        data={images}
+        columnCount={2}
+        renderItem={({ item: { image } }) => (
+          <View style={styles.itemContainer} key={image.id}>
+            <Image
+              source={{ uri: image.urls.small }}
+              style={{
+                width: itemWidth,
+                height: Math.round((itemWidth * image.height) / image.width),
+              }}
+            />
+            <TouchableOpacity style={styles.textContainer}>
+              <Text
+                style={{ maxWidth: itemWidth }}
+                numberOfLines={1}
+                ellipsizeMode="tail"
+              >
+                Author: {image.user.name}
+              </Text>
+            </TouchableOpacity>
+          </View>
+        )}
+      />
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
   },
-  box: {
-    width: 60,
-    height: 60,
-    marginVertical: 20,
+  masonryList: {
+    width: '100%',
+  },
+  itemContainer: {
+    marginTop: 4,
+  },
+  textContainer: {
+    height: 20,
   },
 });
--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,53 @@
-export function multiply(a: number, b: number): Promise<number> {
-  return Promise.resolve(a * b);
+import React, { useEffect, useState } from 'react';
+import { ScrollView, ScrollViewProps, View } from 'react-native';
+
+interface Props<T extends { height: number }>
+  extends Omit<ScrollViewProps, 'children'> {
+  columnCount?: 2 | 3;
+  data: Array<T>;
+  renderItem(props: { item: T }): React.ReactNode;
 }
+
+function MasonryList<T extends { height: number }>(
+  props: Props<T>
+): React.ReactElement {
+  const { columnCount = 2, data, renderItem, ...scrollViewProps } = props;
+  const [splitData, setSplitData] = useState<Array<Array<T>>>();
+
+  useEffect(() => {
+    const offsets = new Array(columnCount).fill(0);
+    const newSplitData: Array<Array<T>> = new Array(columnCount)
+      .fill(0)
+      .map(() => []);
+    for (let item of data) {
+      const minOffset = offsets.reduce((minValue, currentValue) =>
+        minValue > currentValue ? currentValue : minValue
+      );
+      const minOffsetIndex = offsets.findIndex((value) => value === minOffset);
+      newSplitData[minOffsetIndex].push(item);
+      offsets[minOffsetIndex] += item.height;
+    }
+    setSplitData(newSplitData);
+  }, [data, columnCount]);
+
+  return (
+    <ScrollView
+      {...scrollViewProps}
+      style={{ flex: 1 }}
+      contentContainerStyle={{ flexDirection: 'row', justifyContent: 'center' }}
+    >
+      {splitData?.map((items, index) => {
+        return (
+          <View
+            key={`column-${index}`}
+            style={{ flex: 1, marginLeft: index && 5, alignItems: 'center' }}
+          >
+            {items.map((item) => renderItem({ item }))}
+          </View>
+        );
+      })}
+    </ScrollView>
+  );
+}
+
+export default MasonryList;
